44c9c62ce7d99271a9dd1a95908c574e2d7d2137
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2025-03-23 16:06:08 +0300

    Add Assets

---
+++ IL.Daiquiri/Scripts/Runtime/Assets/AssetManager.cs
@@ -0,0 +1,30 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using IL.Daiquiri.Core;
+using JetBrains.Annotations;
+using R3;
+using UnityEngine.AddressableAssets;
+using Object = UnityEngine.Object;
+
+namespace IL.Daiquiri.Assets
+{
+    public sealed class AssetManager
+    {
+        [UsedImplicitly]
+        public AssetManager()
+        {
+        }
+
+        public async UniTask<TObject> LoadAssetAsync<TObject>(string assetKey, LifetimeToken lifetimeToken, CancellationToken cancellationToken)
+            where TObject : Object
+        {
+            var handle = Addressables.LoadAssetAsync<TObject>(assetKey);
+            var asset = await handle.ToUniTask(cancellationToken: cancellationToken, cancelImmediately: true, autoReleaseWhenCanceled: true);
+            var disposable = Disposable.Create(handle, Addressables.Release);
+
+            lifetimeToken.Register(disposable);
+
+            return asset;
+        }
+    }
+}
